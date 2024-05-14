0595391b984828f1fa9f758eb085c054496f7f19
HPerssn <henrik.perssn@gmail.com>
2024-05-14 13:26:22 +0200

    changed ratings to icons

--- src/app/components/MovieList.js
+++ src/app/components/MovieList.js
@@ -1,12 +1,31 @@
 export default function MovieList({ movies, deleteMovie }) {
+  const renderStars = (rating) => {
+    let stars = [];
+    for (let i = 0; i < rating; i++) {
+      stars.push(
+        <img
+          key={i}
+          src="../../images/star.png"
+          alt="star"
+          className="star-image"
+        />
+      );
+    }
+    return stars;
+  };
+
   return (
     <div className="movie-list">
       {movies.map((movie, index) => (
         <div key={index} className="movie-item">
           <h2>{movie.title}</h2>
-          <p>{movie.director}</p>
-          <p>{movie.rating}</p>
-          <button onClick={() => deleteMovie(index)}>Delete</button>
+          <div className="stars">{renderStars(movie.rating)}</div>
+          <img
+            src="../../images/delete.png"
+            alt="delete"
+            onClick={() => deleteMovie(index)}
+            className="delete-image"
+          />
         </div>
       ))}
     </div>
